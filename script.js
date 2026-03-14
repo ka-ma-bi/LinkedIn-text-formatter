5e27d4b319103a2688090213c0a8deca48b83131
ka-ma-bi <kasia.ma.bi@gmail.com>
2026-03-14 17:35:09 +0000

    Update script.js

--- script.js
+++ script.js
@@ -81,10 +81,7 @@ function applyFormat(type) {
             formatted = applyUnicodeMap(normalizedText, unicodeMaps.italic);
         }
     } else if (type === 'underline') {
-        formatted = selectedText.split('\n').map(line => {
-            const len = [...line].length;
-            return line + '\n' + '_'.repeat(len);
-        }).join('\n');
+        formatted = [...selectedText].map(c => c + '\u0332').join('');
     }
 
     const newText = input.value.substring(0, start) + formatted + input.value.substring(end);
@@ -191,7 +188,7 @@ function applyIndent() {
     if (!selectedText) return;
 
     const lines = selectedText.split('\n');
-    const formatted = lines.map((line, i) => i === 0 ? `│ ${line}` : `  ${line}`).join('\n');
+    const formatted = lines.map(line => `│ ${line}`).join('\n');
     const newText = input.value.substring(0, start) + formatted + input.value.substring(end);
     const scrollTop = input.scrollTop;
     input.value = newText;
